6c534b0acdc961c6585d01f2dc30188de3e674fb
k-ogata <visor.ogata@gmail.com>
2019-09-25 13:03:26 +0900

    数字を二桁表示となる様に修正

--- lib/widget/track/track-item.dart
+++ lib/widget/track/track-item.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:track_list/model/track.dart';
 import 'package:sprintf/sprintf.dart';
+import 'package:track_list/model/track.dart';
 
 /// トラック一覧表示
 class TrackItem extends StatelessWidget {
@@ -13,53 +13,54 @@ class TrackItem extends StatelessWidget {
   Widget build(BuildContext context) {
     final int minuteDuration = (this.track.duration / 60).floor();
     final int secondDuration = this.track.duration % 60;
-    return InkWell(
-      child: SafeArea(
+    return SafeArea(
         top: false,
         bottom: false,
-        child: Container(
-          padding: EdgeInsets.fromLTRB(5, 4, 5, 4),
-          decoration: BoxDecoration(
-            border: Border(
-                left: BorderSide(
-                    color: Color.fromARGB(
-                        this.track.duration % 255,
-                        (this.track.duration * 2) % 255,
-                        (this.track.duration * 3) % 255,
-                        (this.track.duration * 4) % 255
-                    ),
-                    width: 8
-                ),
-                bottom: BorderSide(color: Colors.black, width: 2),
+        child: Card(
+          child: Container(
+            padding: EdgeInsets.fromLTRB(5, 4, 5, 4),
+            decoration: BoxDecoration(
+              border: Border(
+                  left: BorderSide(
+                      color: Color.fromARGB(
+                          this.track.duration % 255,
+                          (this.track.duration * 2) % 255,
+                          (this.track.duration * 3) % 255,
+                          (this.track.duration * 4) % 255
+                      ),
+                      width: 8
+                  ),
+                  bottom: BorderSide(color: Colors.black, width: 2),
+              ),
+              //borderRadius: BorderRadius.circular(10),
             ),
-            //borderRadius: BorderRadius.circular(10),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    this.track.title,
+                    textAlign: TextAlign.left,
+                    style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        this.track.artist,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 19.0, color: Colors.grey),
+                      ),
+                      Text(
+                        sprintf('(%02d : %02d)', [minuteDuration, secondDuration]),
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 19.0, color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                ]
+            ),
           ),
-          child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  this.track.title,
-                  textAlign: TextAlign.left,
-                  style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      this.track.artist,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: 19.0, color: Colors.grey),
-                    ),
-                    Text(
-                      '($minuteDuration : $secondDuration)', // TODO sprintfで時間が一けたにならないようにする
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: 19.0, color: Colors.grey),
-                    ),
-                  ],
-                ),
-              ]),
         ),
-      ),
     );
   }
 }
